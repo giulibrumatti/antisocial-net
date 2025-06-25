9614bdba7a44bd86346d5839c105e10b01420c28
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-06-24 23:53:20 -0300

    Add Bootstrap Icons and enhance Profile page with tabs and Post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,49 @@
+import portada from "../../assets/portada.jpg";
+import Card from 'react-bootstrap/Card';
+import { Button } from "react-bootstrap";
+const Post = () => {
+    return (
+        <div className="container">
+            <Card className="w-100 mb-3">
+                <Card.Body>
+                    <Card.Title>Nombre del Usuario</Card.Title>
+                    <Card.Text>
+                        Texto del Post
+                    </Card.Text>
+                </Card.Body>
+                <Card.Img variant="top" src={portada} />
+                <Card.Body className="d-flex justify-content-between">
+                    <Button className="rounded-pill d-flex align-items-center justify-content-around" variant="primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up mx-1" viewBox="0 0 16 16">
+                            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                        </svg>
+                        Like</Button>
+                    <Button className="rounded-pill d-flex align-items-center justify-content-around" variant="primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-repeat mx-1" viewBox="0 0 16 16">
+                            <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
+                        </svg>
+                        Repostear</Button>
+                    <Button className="rounded-pill d-flex align-items-center justify-content-around" variant="primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-square-dots mx-1" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                            <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                        </svg>
+                        Comentar</Button>
+                    <Button className="rounded-pill d-flex align-items-center justify-content-around" variant="primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark mx-1" viewBox="0 0 16 16">
+                            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+                        </svg>
+                        Guardar</Button>
+                    <Button className="rounded-pill d-flex align-items-center justify-content-around" variant="primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share mx-1" viewBox="0 0 16 16">
+                            <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+                        </svg>
+                        Compartir</Button>
+                </Card.Body>
+
+            </Card>
+        </div>
+    );
+}
+
+export default Post;
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import portada from "../../assets/portada.jpg";
 import fotoPerfil from "../../assets/fotoPerfil.png";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import "./profile.css";
+import Post from "../../components/Post/Post";
 const Profile = () => {
+    const [key, setKey] = useState<string>('home');
     const navigate = useNavigate();
     const user = false // se reemplaza con la peticion a useContext para sabver si el usuario esta logueado
     useEffect(() => {
@@ -13,7 +17,7 @@ const Profile = () => {
     }, [user, navigate])
     return (
         <div className="container mx-2 border border-dark-subtle rounded-3 p-0 m-0 w-50">
-            <h4 className="p-2">Profile Page / NOMBRE DEL USUARIO</h4>
+            <h4 className="p-2">NOMBRE DEL USUARIO</h4>
             <div className="p-0 m-0 container-fluid position-relative">
                 <img className="w-100" src={portada} alt="Portada del usuario" />
                 <img className="foto-perfil border border-dark" src={fotoPerfil} alt="" />
@@ -34,6 +38,25 @@ const Profile = () => {
                 <p><span>0</span> Seguidores</p>
                 <p><span>0</span> Siguiendo</p>
             </div>
+            <Tabs
+                defaultActiveKey="Post"
+                id="uncontrolled-tab"
+                className="mb-3"
+                fill
+            >
+                <Tab eventKey="Post" title="Post">
+                    <Post></Post>
+                    <Post></Post>
+                </Tab>
+                <Tab eventKey="Respuesta" title="Respuesta">
+                    <Post></Post>
+                    <Post></Post>
+                </Tab>
+                <Tab eventKey="Destacado" title="Destacado" >
+                    <Post></Post>
+                    <Post></Post>
+                </Tab>
+            </Tabs>
         </div >
     );
 }
