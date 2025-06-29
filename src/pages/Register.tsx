388a50606fe2d0bac40680b200f653d0384c8644
giulibrumatti <giulibrumatti97@gmail.com>
2025-06-29 15:02:43 -0300

    add component Register

---
+++ src/pages/Register.tsx
@@ -0,0 +1,31 @@
+import { Button } from "react-bootstrap";
+
+const Register = () => {
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      <form style={{ width: "100%", maxWidth: "400px" }}>
+        <h2 className="text-center mb-4">Registro de Usuario</h2>
+
+        <div className="mb-3">
+          <label className="form-label">Nombre de usuario</label>
+          <input
+            type="text"
+            className="form-control"
+            id="userName"
+            aria-describedby="username"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Contraseña</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <Button className="rounded-pill w-100 mt-auto" variant="light">
+          Registrar
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
